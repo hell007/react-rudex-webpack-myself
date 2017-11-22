7b4a8aa6d7e8b08b4ef2f4d11ee6ab3c084a02a1
hell007 <1048523306@qq.com>
2017-11-22 10:53:39 +0800

    Update urls.js

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -2,6 +2,8 @@
  * api
  * @type {string}
  */
+const kUrlHost = process.env.BASE_API; 
+
 //文章
 export const kUrlArticleList                = kUrlHost + '/api/articleList';
 export const kUrlArticleView                = kUrlHost + '/api/article';
